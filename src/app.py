9be409b5a22b323db02dfb2fd5a21a26b5607c0e
MILENA1988 <milip19@gmail.com>
2021-02-24 05:31:32 +0000

    finish practice

--- src/app.py
+++ src/app.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify, request, json
 app = Flask(__name__)
 
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text     
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if position < len(todos):
+        todos.pop(position) #pop Saca por el indice y remove saca por el valor
+    else:
+        raise Exception("Sorry, no numbers below zero")
+    return jsonify(todos) , 200
 
 
+  
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
